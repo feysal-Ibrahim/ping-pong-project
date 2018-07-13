ae2cfdd71dcc8b50783cc7c1789db6fa1c6ab8e7
feysal-Ibrahim <addictivefazman@gmail.com>
2018-07-13 21:49:17 +0300

    edited file imdex

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,22 @@
-var pingpong = function(ping) {
-  var numberArray = [];
-  if (ping > 0) {
-    for (var index = 1; index <= ping; index++) {
-      numberArray.push(index);
-    }
-    numberArray.forEach(function(ping) {
-      if (ping % 15 === 0) {
-        numberArray[ping - 1] = 'pingpong';
-      } else if (ping % 3 === 0) {
-        numberArray[ping - 1] = 'ping';
-      } else if (ping % 5 === 0) {
-        numberArray[ping - 1] = 'pong';
+//backend logic here  
+var leapYear = function(year) {
+    return false;
+  };
+  var leapYear = function(year) {
+      if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
+        return true;
+      } else {
+        return false;
       }
-    });
-  }
-  return numberArray;
-}
+    };
 
-var displayResultList = function(outputArray) {
-  $('ul#list').empty();
-  outputArray.forEach(function(item) {
-    $('ul#list').append('<li>' + item + '</li>');
+//user interface logic down here
+  $(document).ready(function() {
+    $('form#pingpong').submit(function(event) {
+      var ping = $('input#ping').val();
+
+      displayResultList(pingpong(ping));
+
+      event.preventDefault();
   });
-}
-
-$(document).ready(function() {
-  $('form#pingpong').submit(function(event) {
-    var ping = $('input#ping').val();
-
-    displayResultList(pingpong(ping));
-
-    event.preventDefault();
-});
+  });
