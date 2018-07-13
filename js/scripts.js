4907b5ae9e69394ed19bfdbee946a03b03737f0b
feysal-Ibrahim <addictivefazman@gmail.com>
2018-07-13 22:40:53 +0300

    modified jquery effect in  js scripts file

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,36 @@
-//backend logic here  
-var leapYear = function(ping) {
-    return ping;
-  };
-  var ping = function() {
-    if ((ping % 3 === 0) & (ping % 5=== 0));
-        return pingpong;
 
-//user interface logic down here
-  $(document).ready(function(pingpong) {
-      $("form#pingpong").submit(function(event) {
-        event.preventDefault();
-        var ping = parseInt($("input#ping").val());
-        var result = pingpong(ping);
-        $("#result").text(result);
-        if (result ===ping);              // same as writing if (result === false)
-           });
-         });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$(document).ready(function() {
+       $("form#pingpong").submit(function(event) {
+           event.preventDefault();
+       var number= parseInt (("input#formNumber").val());
+       pingPong(number);
+   
+       numbersToBePingPonged.forEach(function(number){
+           $("#output").append(""+ number + "");
+       });
+   
+       
+       });
+   
+   });    
+
